867a59fa8fb336629e6bb3bc9cac39d2b633c70c
Clancy-dev <132342072+Clancy-dev@users.noreply.github.com>
2024-12-03 13:25:25 +0300

    menu added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <Header />
       <Button>Hello World</Button>
     </div>
   )
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import { AlignJustify } from "lucide-react";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="menu-icon">
+                <button><AlignJustify /></button>
+            </div>    
+        </header>
+    )
+}
