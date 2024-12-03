5e4de7a564a2a9312c20c4241182696efd1be2ce
Clancy-dev <132342072+Clancy-dev@users.noreply.github.com>
2024-12-03 12:27:47 +0300

    shadcn components with button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      Hello World !
+      <Button>Hello World</Button>
     </div>
   )
 }
